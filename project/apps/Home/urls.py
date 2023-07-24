e7ceda8c0ea4f7b8976ec18fe09c29414b3064d4
Santiago Goldstein <santiago3237@hotmail.com>
2023-07-24 18:54:09 -0300

    add: login al proyecto

--- project/apps/Home/urls.py
+++ project/apps/Home/urls.py
@@ -8,6 +8,7 @@ app_name = "Home"
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("login/", views.login_request, name="login"),
     path("about/", TemplateView.as_view(template_name="Home/about.html"), name="about"),
 ]
 
