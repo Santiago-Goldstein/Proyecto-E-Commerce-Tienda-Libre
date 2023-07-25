694aac5a7f9efc1eeb774f4da6f77471b3420315
Santiago Goldstein <santiago3237@hotmail.com>
2023-07-25 11:10:30 -0300

    add: Logout

--- project/apps/Home/urls.py
+++ project/apps/Home/urls.py
@@ -1,6 +1,7 @@
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
 from django.views.generic import TemplateView
+from django.contrib.auth.views import LogoutView
 
 from . import views
 
@@ -9,6 +10,7 @@ app_name = "Home"
 urlpatterns = [
     path("", views.home, name="home"),
     path("login/", views.login_request, name="login"),
+    path("logout/", LogoutView.as_view(template_name="Home/logout.html"), name="logout"),
     path("about/", TemplateView.as_view(template_name="Home/about.html"), name="about"),
 ]
 
