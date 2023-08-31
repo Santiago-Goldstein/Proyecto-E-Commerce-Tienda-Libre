aa44237590c617dbdd690e77a3c9916aef30ba18
Santiago Goldstein <santiago3237@hotmail.com>
2023-08-31 00:37:38 -0300

    a

--- project/config/settings.py
+++ project/config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = "mykey43599452"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['main--tiendalibre.netlify.app']
 
 
 # Application definition
